85b47864970d3269a1fec387cef4ac838f6c10a1
Viiuuuu <nguyenhavyy0105@gmail.com>
2025-09-27 23:02:36 +0700

    Commit lan thu 4: Them code cho file AndroidManifest

--- app/src/main/java/com/example/test/Login.java
+++ app/src/main/java/com/example/test/Login.java
@@ -1,4 +1,35 @@
 package com.example.test;
 
-public class Login {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin;
+    private TextView tvForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityindex); // dùng đúng tên layout của bạn
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String pass = edtPassword.getText().toString();
+                Toast.makeText(Login.this, "Đã bấm đăng nhập: " + email, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
